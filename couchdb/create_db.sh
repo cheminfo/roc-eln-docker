cfdb9e2b4b81f6199fbc49a2e9471694213742f2
Michaël Zasso <targos@protonmail.com>
2016-12-15 11:34:42 +0100

    couchdb: add default groups to databases

--- couchdb/create_db.sh
+++ couchdb/create_db.sh
@@ -15,11 +15,29 @@ curl -X POST http://localhost:5984/_users \
      -d '{ "_id": "org.couchdb.user:admin@cheminfo.org", "name": "admin@cheminfo.org", "type": "user", "roles": [], "password": "restoncouch77" }'
 
 curl -X PUT http://localhost:5984/eln
+curl -X POST http://localhost:5984/eln \
+     -H 'Content-Type: application/json' \
+     -d '{"$type": "group", "$owners": ["admin@cheminfo.org"], "name": "anonymousRead", "users": [], "rights": ["read"], "$lastModification": "admin@cheminfo.org", "$modificationDate": 0, "$creationDate": 0}';
+curl -X POST http://localhost:5984/eln \
+     -H 'Content-Type: application/json' \
+     -d '{"$type": "group", "$owners": ["admin@cheminfo.org"], "name": "anyuserRead", "users": [], "rights": ["read"], "$lastModification": "admin@cheminfo.org", "$modificationDate": 0, "$creationDate": 0}';
+curl -X PUT http://localhost:5984/visualizer/defaultGroups \
+     -H 'Content-Type: application/json' \
+     -d '{"_id": "defaultGroups","$type": "db","anonymous": ["anonymousRead"],"anyuser": ["anyuserRead"]}'
 curl -X PUT http://localhost:5984/eln/_security \
      -H 'Content-Type: application/json' \
      -d '{ "admins": { "names": ["rest-on-couch"], "roles": [] }, "members": { "names": ["rest-on-couch"], "roles": [] } }'
 
 curl -X PUT http://localhost:5984/visualizer
+curl -X POST http://localhost:5984/visualizer \
+     -H 'Content-Type: application/json' \
+     -d '{"$type": "group", "$owners": ["admin@cheminfo.org"], "name": "anonymousRead", "users": [], "rights": ["read"], "$lastModification": "admin@cheminfo.org", "$modificationDate": 0, "$creationDate": 0}';
+curl -X POST http://localhost:5984/visualizer \
+     -H 'Content-Type: application/json' \
+     -d '{"$type": "group", "$owners": ["admin@cheminfo.org"], "name": "anyuserRead", "users": [], "rights": ["read"], "$lastModification": "admin@cheminfo.org", "$modificationDate": 0, "$creationDate": 0}';
+curl -X PUT http://localhost:5984/visualizer/defaultGroups \
+     -H 'Content-Type: application/json' \
+     -d '{"_id": "defaultGroups","$type": "db","anonymous": ["anonymousRead"],"anyuser": ["anyuserRead"]}'
 curl -X PUT http://localhost:5984/visualizer/_security \
      -H 'Content-Type: application/json' \
      -d '{ "admins": { "names": ["rest-on-couch"], "roles": [] }, "members": { "names": ["rest-on-couch"], "roles": [] } }'
