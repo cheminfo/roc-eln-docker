8461ecb5420589e91fb8972816caf3ae6850f70a
Michaël Zasso <targos@protonmail.com>
2016-12-15 11:00:53 +0100

    Update roc-config.js

--- roc-config.js
+++ roc-config.js
@@ -1,20 +1,28 @@
 'use strict';
 
 module.exports = {
+  // Change this to the base address of your server
   allowedOrigins: ['http://server.example.com'],
+  // Same as above, without http://
   sessionDomain: 'server.example.com',
+  // put a 32 character random string
   keys: ['changeThisForARandomKey'],
   logLevel: 'FATAL',
+  
+  administrators: ['admin@cheminfo.org'],
 
-  // CouchDB credentials
-  url: 'http://couchdb:5984',
-  username: "rest-on-couch",
-  password: "restoncouch77",
+  url: 'http://couchdb:5984', // do not change
+  username: "rest-on-couch", // do not change
+  password: "password", // put the same password as in docker-compose.yml
 
-  port: 3000,
+  port: 3000, // do not change
+  // if your application is behind a reverse proxy, add the prefix here (but keep /roc/ at the end)
+  // example: app is available at http://server.example.com/my/app. proxyPrefix must be '/my/app/roc/'
   proxyPrefix: '/roc/',
-  publicAddress: 'http://server.example.com/roc/',
+  // main address from allowedOrigins
+  publicAddress: 'http://server.example.com/',
   auth: {
+    /* uncomment and configure for ldap login
     ldap: {
       server: {
         url: 'ldaps://ldap.epfl.ch',
@@ -28,6 +36,14 @@ module.exports = {
         };
       }
     },
+    */
+    /* uncomment and configure for google login
+    google: {
+      clientID: "mylongclientid.apps.googleusercontent.com",
+      clientSecret: "myclientsecret"
+    },
+    */
+    // do not change
     couchdb: {
       showLogin: false
     }
