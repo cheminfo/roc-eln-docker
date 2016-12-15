b599c25aef817152ac5784434c23d138a2660da9
Michaël Zasso <targos@protonmail.com>
2016-12-15 13:16:04 +0100

    allow to run CouchDB as root

--- couchdb/start_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -n "$(find /usr/local/var/lib/couchdb -prune -empty)" ]
-then
-    echo "Initialize CouchDB data"
-    /bin/bash /create_db.sh
-fi
-
-/bin/bash /docker-entrypoint.sh couchdb
