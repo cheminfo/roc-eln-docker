bf7c0cb0fbbc6c8a6a4d84c97afd92a874d7ae7c
Michaël Zasso <targos@protonmail.com>
2016-12-16 14:28:20 +0100

    isolate constant values of roc-config
    Closes #2

--- roc-config.js
+++ roc-config.js
@@ -7,15 +7,12 @@ module.exports = {
   sessionDomain: 'server.example.com',
   // put a 32 character random string
   keys: ['changeThisForARandomKey'],
-  logLevel: 'FATAL',
   
+  // keep admin@cheminfo.org. You can add more admins if you want
   administrators: ['admin@cheminfo.org'],
-
-  url: 'http://couchdb:5984', // do not change
-  username: "rest-on-couch", // do not change
-  password: "password", // put the same password as in docker-compose.yml
-
-  port: 3000, // do not change
+  
+  password: "password", // put the COUCHDB_PASSWORD from docker-compose.yml
+  
   // if your application is behind a reverse proxy, add the prefix here (but keep /roc/ at the end)
   // example: app is available at http://server.example.com/my/app. proxyPrefix must be '/my/app/roc/'
   proxyPrefix: '/roc/',
@@ -43,7 +40,7 @@ module.exports = {
       clientSecret: "myclientsecret"
     },
     */
-    // do not change
+    // do not disable couchdb login. You can enable "showLogin" if necessary
     couchdb: {
       showLogin: false
     }
@@ -55,5 +52,12 @@ module.exports = {
     read: [],
     write: [],
     create: ['anyuser']
-  }
+  },
+  
+  
+  // From here, DO NOT CHANGE ANYTHING unless you know what you are doing!
+  logLevel: 'FATAL',
+  port: 3000,
+  url: 'http://couchdb:5984',
+  username: "rest-on-couch"
 };
