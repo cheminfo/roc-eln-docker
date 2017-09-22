80bd31a5a5dcbeb100cf62b5ab1ccbd981f72eb9
Michaël Zasso <targos@protonmail.com>
2017-09-22 10:41:41 +0200

    remove apache from flavor-builder

--- flavor-builder/start.sh
+++ flavor-builder/start.sh
@@ -2,8 +2,9 @@
 
 printenv | sed 's/^\(.*\)$/export \1/g' > /env.sh
 
-service apache2 start
 service cron start
 echo "started services"
+
+node /git/flavor-builder/node_modules/visualizer-on-tabs/bin/build.js --outDir=/var/www/html/on-tabs/ --config=/on-tabs-config.json
 
 tail -f /dev/null
