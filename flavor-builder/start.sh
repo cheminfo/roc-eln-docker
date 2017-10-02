dd0f9912730ccf9c6c2067bb1ab21fa3a7d90905
Luc Patiny <luc@patiny.com>
2017-10-02 17:47:45 +0200

    follow cron.log in start.sh

--- flavor-builder/start.sh
+++ flavor-builder/start.sh
@@ -7,4 +7,4 @@ echo "started services"
 
 node /git/flavor-builder/node_modules/visualizer-on-tabs/bin/build.js --outDir=/var/www/html/on-tabs/ --config=/on-tabs-config.json
 
-tail -f /dev/null
+tail -f /var/log/cron.log
