000040b7a267af647ea3c3b1ec04a226bbb6ae3a
Daniel Kostro <kostro.d@gmail.com>
2022-05-02 09:46:37 +0200

    chore: add better example of how to use getUserInfo and add getPublicUserInfo

--- rest-on-couch-home/ldap.js
+++ rest-on-couch-home/ldap.js
@@ -6,12 +6,30 @@ module.exports = undefined;
   server: {
     url: 'ldaps://ldap.example.com',
     searchBase: 'c=ch',
-    searchFilter: 'uid={{username}}'
+    searchFilter: 'uid={{username}}',
   },
-  getUserInfo: function(user) {
+  getUserInfo: async function (user) {
+    function pickUserData(user) {
+      return {
+        uid: user.uid[0],
+        group: user.uid[1].replace(/^.*@/, ''),
+      };
+    }
+    // Will be string when data userInfo/_me api called
+    if (typeof user === 'string') {
+      // ldapSearch function is preconfigured with the ldap auth options (binding credentials, domain)
+      // They can all be overriden
+      const user = await ldapSearch({ filter: `mail:${user}` });
+      return pickUserData(user);
+    }
+    // Will be object when called in authentication workflow
+    else {
+      return pickUserData(user);
+    }
+  },
+  getPublicUserInfo(user) {
     return {
-      uid: user.uid[0],
-      group: user.uid[1].replace(/^.*@/, '')
+      displayName: user.displayName,
     };
-  }
+  },
 };*/
